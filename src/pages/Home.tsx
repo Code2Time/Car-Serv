2371cf66b723b1dae220fb3d090c0631f665d6ba
Linux-user <code2time@gmail.com>
2024-10-07 18:15:15 +0330

    Add some assets(img , font)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 function Home() {
   return (
-    <h1 className="text-red-600 text-3xl ">
+    <h1 className="playwrite3 text-red-500 text-center uppercase mt-3 text-9xl ">
       home 
     </h1>
   )
