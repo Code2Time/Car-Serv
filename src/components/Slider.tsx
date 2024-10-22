eec734af05f6f1313d05257c7faaa7c4888d37fe
Linux-user <code2time@gmail.com>
2024-10-22 19:57:01 +0330

    install swiperjs and update slider component

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,38 +1,30 @@
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
-import Slider from "react-slick";
-function FirstSlider() {
-    const settings = {
-        dots: true,
-        fade: true,
-        infinite: true,
-        speed: 600,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        waitForAnimate: false
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+// import images
 
-      };
-      return (
-        <div className="slider-container">
-          <Slider {...settings}>
-            <div>
-              <img src='https://media1.thrillophilia.com/filestore/8vh1qgvmud08c5vm2goj4aucretr_ew41nl9hgdajdas55cjfe02isgid_119.jpg' />
+function FirstSlider() {
+  return (
+    <>
+      <div className="slider-container h-1/2 sm:h-screen">
+        <Swiper
+          pagination={{
+            dynamicBullets: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="first-slider-item size-full">
+             {/* <img src={slidbg1} rel="preload" alt="slider-background" /> */}
             </div>
-            <div>
-              <img src='https://4kwallpapers.com/images/walls/thumbs_3t/19145.jpg' />
-            </div>
-            {/* <div>
-              <img src={baseUrl + "/abstract02.jpg"} />
-            </div>
-            <div>
-              <img src={baseUrl + "/abstract03.jpg"} />
-            </div>
-            <div>
-              <img src={baseUrl + "/abstract04.jpg"} />
-            </div> */}
-          </Slider>
-        </div>
-      );
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </>
+  );
 }
 
-export default FirstSlider
+export default FirstSlider;
